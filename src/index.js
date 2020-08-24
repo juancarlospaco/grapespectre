aa2a1f565f0b6d1edd0589a7d71b6d28ec8f15af
Juan Carlos <JuanCarlospaco@gmail.com>
2020-08-24 13:56:34 -0300

    update

--- src/index.js
+++ src/index.js
@@ -18,11 +18,5 @@ export default (editor, options = {}) => {
     openBl && openBl.set('active', 1);
     editor.Blocks.getCategories().filter(c => ['Spectre➡️Icons', 'Spectre➡️Buttons', 'Spectre➡️Experimentals', 'Spectre➡️Utils', 'Spectre➡️Media'].includes(c.get('id'))).forEach(c => c.set('open', 0));
 
-    document.querySelector("div.gjs-cm-editor-c").style.width = "99%";
-    document.querySelector("div.gjs-cm-editor#gjs-cm-htmlmixed").style.border = 0;
-    document.querySelector("div.gjs-cm-editor#gjs-cm-css").style.display = "none";
-    document.querySelector("div.gjs-cm-editor#gjs-cm-htmlmixed").style.padding = 0;
-    document.querySelector("span.gjs-pn-btn.fa.fa-code").style.background = "limegreen";
-
   })
 };
